6fca3a95a9c68789fc3a57ca13d2c8ec057a3a98
Flyingdotschman <dominik.poeschel@hotmail.com>
2021-04-29 13:12:41 +0200

    get_wlan0IP.py added 2te ösung added

--- get_wlan0IP.py
+++ get_wlan0IP.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import json
 gw = os.popen("ip -4 route show default").read().split()
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((gw[2], 0))
@@ -7,3 +8,13 @@ ipaddr = s.getsockname()[0]
 gateway = gw[2]
 host = socket.gethostname()
 print("IP:", ipaddr, " GW:", gateway, " Host:", host)
+
+
+
+routes = json.loads(os.popen("ip -j -4 route")).read()
+
+for r in routes:
+    if r.get("dev") == "wlan0" and r.get("prefsrc"):
+        ip = r['prefsrc']
+        continue
+print(f"IP: {ip}")
